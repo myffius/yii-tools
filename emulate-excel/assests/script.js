827477b803174a8caeaaa5b40484c1b7a587816d
myffius <andryuhab@yandex.ru>
2012-12-25 16:59:54 +0400

    Коммитим, коммитим ...

--- emulate-excel/assests/script.js
+++ emulate-excel/assests/script.js
@@ -1,27 +1,30 @@
 jQuery(document).ready(function(){
     jQuery.fn.clearFormat = function(){
-        this.removeClass('format-percent').removeClass('format-number').removeClass('format-text');
+        this.removeClass('format-percent')
+            .removeClass('format-number')
+            .removeClass('format-text');
+        return this;
     }
 
     jQuery('.excel-table td').click(function(){
-        editabledCell = $('.excel-table td.selected input');
+        editabledCell = jQuery('.excel-table td.selected input');
         if (editabledCell.length > 0){
-            editabledCell.parent('td').html('<div>' + $(this).val() +'<span></span></div>')
+            editabledCell.parent('td').html('<div>' + jQuery(this).val() +'<span></span></div>')
         }
-        $('.excel-table td, .excel-table th').removeClass('selected');
-        $(this).addClass('selected').parent().children('th').addClass('selected');
+        jQuery('.excel-table td, .excel-table th').removeClass('selected');
+        jQuery(this).addClass('selected').parent().children('th').addClass('selected');
         //$('.excel-table tr.evaluate-line input').val($(this).text());
     })
 
     jQuery('.excel-table td').dblclick(function(){
         text = $(this).text();
-        $(this).html('<div style="padding: 0; width: 86px;"><input style="position: relative;padding-left: 3px; top: 0; left: 0; border: none; outline: none; width: 74px" value="'+text+'" /></div>');
+        jQuery(this).html('<div style="padding: 0; width: 86px;"><input style="position: relative;padding-left: 3px; top: 0; left: 0; border: none; outline: none; width: 74px" value="'+text+'" /></div>');
     })
 
     jQuery(document).keyup(function(eventObject){
-        if ($('.excel-table td.selected input').length > 0)
+        if (jQuery('.excel-table td.selected input').length > 0)
             return false;
-        current = $('.excel-table td.selected');
+        current = jQuery('.excel-table td.selected');
         if (eventObject.which == 39 && !current.next().is(':last'))
             current.removeClass('selected').next().addClass('selected');
         if (eventObject.which == 37 && !current.prev().is('th'))
